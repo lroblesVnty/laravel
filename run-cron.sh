fdcdf7f58d1256614df95da2c6a359c2327a4050
LuisRobles <lrobles.vnty@gmail.com>
2025-09-16 13:54:20 -0600

    refactor run-cron.sh view logs in railway

--- run-cron.sh
+++ run-cron.sh
@@ -2,9 +2,9 @@
 # Make sure this file has executable permissions, run `chmod +x run-cron.sh`
 
 # This block of code runs the Laravel scheduler every minute
-while [ true ]
-    do
-        echo "Running the scheduler..."
-        php artisan schedule:run --verbose --no-interaction &
-        sleep 60
-    done
+while true
+do
+  echo "[CRON] Ejecutando scheduler: $(date)" >> storage/logs/cron.log
+  php artisan schedule:run --verbose --no-interaction >> storage/logs/cron.log 2>&1
+  sleep 60
+done
