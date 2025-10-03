72f52fb89e243f65d10b042fc9b1760ef511eeb9
LuisRobles <lrobles.vnty@gmail.com>
2025-10-03 10:56:11 -0600

    fix: add conditional check for ROLE in run-cron.sh to ensure proper execution of the Laravel scheduler

--- run-cron.sh
+++ run-cron.sh
@@ -2,9 +2,10 @@
 # Make sure this file has executable permissions, run `chmod +x run-cron.sh`
 
 # This block of code runs the Laravel scheduler every minute
-while true
-do
-  echo "[CRON] Ejecutando scheduler: $(date)" | tee -a storage/logs/cron.log
-  php artisan schedule:run --verbose --no-interaction 2>&1 | tee -a storage/logs/cron.log
-  sleep 60
-done
+if [ "$ROLE" = "cron" ]; then
+  while true
+  do
+    echo "[CRON] Ejecutando scheduler: $(date)" | tee -a storage/logs/cron.log
+    php artisan schedule:run --verbose --no-interaction 2>&1 | tee -a storage/logs/cron.log
+    sleep 60
+  done
